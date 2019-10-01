0fbda7c9209aa80bbe89be7f5bda05838d9f26a0
Boris Mandzhukov <skipter@abv.bg>
2019-10-01 21:12:04 +0300

    Library testing

---
+++ CharCounter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CharCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            long charCounter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                charCounter++;
+            }
+            Console.WriteLine(charCounter);
+        }
+    }
+}
