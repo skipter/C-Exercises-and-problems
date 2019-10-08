8b76b67cb2193562d23bf00f0f9e2c7e3ba82375
Boris Mandzhukov <skipter@abv.bg>
2019-10-08 17:27:17 +0300

    Add dictionary exercise

---
+++ FixEmailsDictionary/FixEmails.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FixEmailsDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> data = new Dictionary<string, string>();
+
+            string input;
+
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input == "stop")
+                {
+                    break;
+                }
+
+                string email = Console.ReadLine();
+                 
+                if (!data.ContainsKey(input))
+                {
+                    if (!email.Contains(".us") || email.Contains(".uk"))
+                    {
+                        data.Add(input, email);
+                    }
+                }
+            }
+
+            foreach (var user in data)
+            {
+                Console.WriteLine("{0} -> {1}", user.Key, user.Value);
+            }
+        }
+    }
+}
---
+++ PopulationCounter/PopulationCounter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PopulationCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, long>> dataPopulation = new Dictionary<string, Dictionary<string, long>>();
+
+            string incomeData = Console.ReadLine();
+
+            while (incomeData != "report")
+            {
+                string[] tokens = incomeData.Split('|').ToArray();
+
+                string country = tokens[0];
+                string city = tokens[1];
+                long population = Int64.Parse(tokens[2]);
+
+                if (!dataPopulation.ContainsKey(country))
+                {
+                    dataPopulation.Add(country, new Dictionary<string, long>());
+                    dataPopulation[country].Add(city, population);
+                }
+            }
+        }
+    }
+}
