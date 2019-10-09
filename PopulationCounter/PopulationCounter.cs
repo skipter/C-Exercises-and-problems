7554b2d83ef405bf87674e31754fc702e7aa8840
Boris Mandzhukov <skipter@abv.bg>
2019-10-09 20:39:43 +0300

    Add dictionry exercise, Country Population

--- PopulationCounter/PopulationCounter.cs
+++ PopulationCounter/PopulationCounter.cs
@@ -16,14 +16,41 @@ namespace PopulationCounter
             {
                 string[] tokens = incomeData.Split('|').ToArray();
 
-                string country = tokens[0];
-                string city = tokens[1];
+                string country = tokens[1];
+                string city = tokens[0];
                 long population = Int64.Parse(tokens[2]);
 
                 if (!dataPopulation.ContainsKey(country))
                 {
                     dataPopulation.Add(country, new Dictionary<string, long>());
                     dataPopulation[country].Add(city, population);
+                }
+
+                if (dataPopulation.ContainsKey(country))
+                {
+                    if (!dataPopulation[country].ContainsKey(city))
+                    {
+                        dataPopulation[country].Add(city, 0);
+                    }
+                    else
+                    {
+                        dataPopulation[country][city] = population;
+                    }
+                }
+
+                incomeData = Console.ReadLine();
+            }
+
+            foreach (var item in dataPopulation.OrderByDescending(x => x.Value.Values.Sum()))
+            {
+                var country = item.Key;
+                Console.WriteLine("{0} (total population: {1})", country, item.Value.Values.Sum());
+
+                foreach (var items in item.Value.OrderByDescending(p => p.Value))
+                {
+                    var city = items.Key;
+                    long popUp = items.Value;
+                    Console.WriteLine($"=>{city}: {popUp}");
                 }
             }
         }
